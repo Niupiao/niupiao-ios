af06ce7b03d90e5c685b34bd8a1141fd08834266
Kevin Chen <meejathoughts@gmail.com>
2015-03-06 23:03:46 -0500

    added URL constants

---
+++ Niupiao/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  Niupiao
+//
+//  Created by Kevin Chen on 3/6/15.
+//  Copyright (c) 2015 Niupiao. All rights reserved.
+//
+
+import Foundation
+
+class Constants {
+    
+    struct Url {
+        static let BASE_URL = "https://niupiao-rails.herokuapp.com"
+        static let LOGIN_URL = BASE_URL + "/login.json"
+        static let EVENTS_URL = BASE_URL + "/events.json"
+        static let TICKETS_URL = BASE_URL + "/me/tickets.json"
+    }
+    
+}
